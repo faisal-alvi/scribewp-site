08ba33db3be0f277409cd9d8385dcb6344555f6a
faisal-alvi <faisalalvi33@gmail.com>
2026-01-10 17:40:56 +0530

    remove unnecessary code and related functions

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT;
 
 app.get('/', (req, res) => {
   res.send('Freemius proxy server — endpoints: GET /freemius, POST /create-checkout');
